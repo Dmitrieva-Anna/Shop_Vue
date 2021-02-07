bb86f4fb013198b44364818ec3f8806994c2a12e
Dmitrieva-Anna <anna_danilova_93@mail.ru>
2021-02-07 18:35:45 +0300

    Second changes
    added footer and more products, style upgraded, calc value of cart

---
+++ CartItem.js
@@ -0,0 +1,15 @@
+export const CartItem = {
+    props: ['img', 'cartItem'],
+    template: `<div class="product-bio">
+                    <img class="cart-item" :src="img + cartItem.product_name + '.jpg'" :alt="cartItem.product_name">
+                    <div class="product-desc">
+                        <p class="product-title">{{ cartItem.product_name }}</p>
+                        <p class="product-quantity">Количество: {{ cartItem.quantity }}</p>
+                        <p class="product-single-price">{{ cartItem.price }} &#8381</p>
+                    </div>
+                    <div class="right-block">
+                        <p class="product-price">{{ cartItem.quantity * cartItem.price }} &#8381</p>
+                        <button class="del-btn" @click="$emit('removeProduct', cartItem)">&times;</button>
+                    </div>
+                </div>`
+}
---
+++ Products.js
@@ -0,0 +1,49 @@
+import { Product } from "./Product.js";
+
+export const Products = {
+    inject: ['API', 'getJson'],
+    components: {
+        Product,
+        // Footer,
+    },
+    data() {
+        return {
+            catalogUrl: '/catalogData.json',
+            products: [],
+            imgCatalog: 'imgs/',
+            // imgCatalog: 'https://placehold.it/200x150',
+        }
+    },
+    computed: {
+        filtered() {
+            return this.products.filter(item => new RegExp(this.$root.$refs.search.searchProduct, 'i').test(item.product_name));
+        }
+    },
+    mounted() {   // получить данные с сервера и добавить их в соотв. массивы товаров
+        this.$root.getJson(`${this.API + this.catalogUrl}`)
+            .then(data => {
+                if (!data) {
+                    return;
+                }
+                for (let product of data) {
+                    this.products.push(product);
+                }
+            });
+        this.$root.getJson(`getProducts.json`)
+            .then(data => {
+                if (!data) {
+                    return;
+                }
+                for (let product of data) {
+                    this.products.push(product);
+                }
+            });
+    },
+    template: ` <div class="products">
+                    <Product v-for="el of filtered" 
+                        :key="el.id_product"
+                        :img="imgCatalog"
+                        :product="el">
+                    </Product>
+                </div>`
+} 
